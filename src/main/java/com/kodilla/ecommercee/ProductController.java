637411e26b0891679947fc9874cb0db2cda4c7a4
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-02-26 00:02:24 +0100

    Endpoints implementation methods created

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,33 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("v1/product")
 public class ProductController {
+
+    @GetMapping(name = "getAllProducts")
+    public void getAllProducts() {
+        System.out.println("returns list of all products");
+    }
+
+    @GetMapping(name = "getProduct")
+    public void getProduct(Long id) {
+        System.out.println("returns one product with provided " + id);
+    }
+
+    @PostMapping(name = "createProduct")
+    public void createProduct() {
+        System.out.println("creates a product");
+    }
+
+    @PutMapping(name = "updateProduct")
+    public void updateProduct(Long id) {
+        System.out.println("updates a product data with provided " + id);
+    }
+
+    @DeleteMapping(name = "deleteProduct")
+    public void deleteProduct(Long id) {
+        System.out.println("deletes a product with provided " + id);
+    }
 }
