fc178c23199bcf2373177daf73a34f903114a791
sebalaka <sebalaka@gmail.com>
2021-02-26 20:16:28 +0100

    JDP210202-5

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,4 +1,26 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/user")
 public class UserController {
+
+    @RequestMapping(method = RequestMethod.POST, value = "createUser", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public UserDto createUser(@RequestBody UserDto userDto) {
+        return new UserDto(userDto.getUserId(), "Giuseppe", "goodPeople@gmail.com","noH4ck@bbl3",123,true);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "banUser")
+    public void blockUser(@RequestParam int userId) {
+        System.out.println("zmienna.getUserId.setStatus");
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createUserKey")
+    public void createUserKey(@RequestParam String username, @RequestParam String password) {
+        System.out.println("user.setSecretUserKey");
+    }
 }
+
+
--- src/main/java/com/kodilla/ecommercee/UserDto.java
+++ src/main/java/com/kodilla/ecommercee/UserDto.java
@@ -1,9 +1,15 @@
 package com.kodilla.ecommercee;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
     private int userId;
     @NotBlank (message = "FirstName cannot be blank")
@@ -13,6 +19,6 @@ public class UserDto {
     @NotBlank
     @Length(min = 6, max = 16)
     private String password;
-    private int key;
+    private int tokenUserKey;
     private boolean status;
 }
