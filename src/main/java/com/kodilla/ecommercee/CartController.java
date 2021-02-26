dc4f702ddd29166fdf1e3737fd5606708fcdbc9a
Rafal Krismann <rkrismann33@gmail.com>
2021-02-26 09:17:53 +0000

    JDP210202-3 - ready for review 3

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/v1/cart")
 public class CartController {
@@ -12,28 +16,31 @@ public class CartController {
     public CartController(){}
 
     @RequestMapping(method = RequestMethod.POST, value = "newCart", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void newCart(int id) {
-        System.out.println("koszyk" + id + " stworzony");
+    public CartDto newCart(int id) {
+        return new CartDto(id);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getProductFromEmptyCart")
-    public void getProductFromEmptyCart() {
-        System.out.println("elementy pobrane???");
+    public ProductDto getProductFromEmptyCart() {
+        return new ProductDto(1,"pralka",899,"najnowsza i tak dalej");
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "addProduct", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void addProduct(int id) {
-        System.out.println("produkt" + id + " dodany");
+        List<ProductDto> products = new LinkedList<>();
+        ProductDto product = new ProductDto(1,"pralka",899,"najnowsza i tak dalej");
+        products.add(product);
+        System.out.println(product + " dodany");
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
-    public void removeProduct() {
-        System.out.println("produkt usuniety");
+    @RequestMapping(method = RequestMethod.DELETE, value = "removeProduct")
+    public void removeProduct(int id) {
+        System.out.println("produkt" + id +" usuniety");
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createOrder", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void createOrder(int id) {
-        System.out.println("zamowienie" + id + " stworzone");
+    public OrderDto createOrder(int id) {
+        return new OrderDto(id,new Date());
     }
 
 
