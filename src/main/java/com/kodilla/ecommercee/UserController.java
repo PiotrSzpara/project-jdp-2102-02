d36fe39bb54d9ef614d2649b5befc1417c7e7d4f
sebalaka <sebalaka@gmail.com>
2021-02-26 20:51:57 +0100

    JDP210202-5 fix

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -12,7 +12,7 @@ public class UserController {
         return new UserDto(userDto.getUserId(), "Giuseppe", "goodPeople@gmail.com","noH4ck@bbl3",123,true);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "banUser")
+    @RequestMapping(method = RequestMethod.PUT, value = "blockUser")
     public void blockUser(@RequestParam int userId) {
         System.out.println("zmienna.getUserId.setStatus");
     }
