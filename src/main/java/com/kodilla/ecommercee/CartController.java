fdba6c0295befa2ae5e77e268dd0327a27186d80
Rafal Krismann <rkrismann33@gmail.com>
2021-02-26 09:25:35 +0000

    JDP210202-3 - ready for review 3

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -16,31 +16,31 @@ public class CartController {
     public CartController(){}
 
     @RequestMapping(method = RequestMethod.POST, value = "newCart", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public CartDto newCart(int id) {
-        return new CartDto(id);
+    public CartDto newCart(int cartId) {
+        return new CartDto(cartId);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getProductFromEmptyCart")
-    public ProductDto getProductFromEmptyCart() {
-        return new ProductDto(1,"pralka",899,"najnowsza i tak dalej");
+    public ProductDto getProductFromEmptyCart(int productId) {
+        return new ProductDto(productId,"pralka",899,"najnowsza i tak dalej");
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "addProduct", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void addProduct(int id) {
+    public void addProduct(int productId) {
         List<ProductDto> products = new LinkedList<>();
-        ProductDto product = new ProductDto(1,"pralka",899,"najnowsza i tak dalej");
+        ProductDto product = new ProductDto(productId,"pralka",899,"najnowsza i tak dalej");
         products.add(product);
         System.out.println(product + " dodany");
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "removeProduct")
-    public void removeProduct(int id) {
-        System.out.println("produkt" + id +" usuniety");
+    public void removeProduct(int productId) {
+        System.out.println("produkt" + productId +" usuniety");
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createOrder", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public OrderDto createOrder(int id) {
-        return new OrderDto(id,new Date());
+    public OrderDto createOrder(int orderId) {
+        return new OrderDto(orderId,new Date());
     }
 
 
