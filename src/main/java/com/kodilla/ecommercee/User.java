b0c39c501ed5e6c85137e547034fb45ae719524a
sebalaka <sebalaka@gmail.com>
2021-02-28 17:57:19 +0100

    JDP212020-15 add User Entity

--- src/main/java/com/kodilla/ecommercee/User.java
+++ src/main/java/com/kodilla/ecommercee/User.java
@@ -1,20 +1,39 @@
 package com.kodilla.ecommercee;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "user")
+@Builder
 public class User {
-
     @Id
     @GeneratedValue
+    @NotNull
+    @Column(name = "USERID", unique = true)
     private int userId;
+    @Column(name = "USERNAME")
+    private String userName;
+    @NotNull
+    @Column(name = "EMAIL")
+    private String email;
+    @NotNull
+    @Column(name = "PASSWORD")
+    private String password;
+    @Column(name = "KEY")
+    private String tokenUserKey;
+    @OneToMany(
+            targetEntity = Cart.class,
+            mappedBy = "carts",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public Cart cart;
 }
