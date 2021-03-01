f38270c34b70d6630fab8be7bb13a72c766bbed5
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-03-01 20:57:23 +0100

    Packages created, classes sorted. Unnecessary OrderDto class deleted

--- src/main/java/com/kodilla/ecommercee/domain/GroupDao.java
+++ src/main/java/com/kodilla/ecommercee/domain/GroupDao.java
@@ -1,13 +1,12 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.Group;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Transactional
 @Repository
-public interface ProductDao extends CrudRepository <Product, Integer> {
-    List<Product> findByProductName(String productName);
+public interface GroupDao extends CrudRepository<Group, Integer> {
+
 }
