9c91c60e1051ef00d8eb9c4d3fe348a71dfb8377
Kamil Mikołajczyk <kamilmik2016@gmail.com>
2021-03-02 16:48:19 +0100

    Resolving conflicts with master

---
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -0,0 +1,67 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "GROUP")
+@NoArgsConstructor
+public class Group {
+    private int groupId;
+    private String name;
+    private String description;
+    private List<Product> products = new ArrayList<>();
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "GROUP_ID", unique = true)
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    @NotNull
+    @Length(max = 45)
+    @Column(name = "NAME", length = 45)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Length(max = 45)
+    @Column(name = "DESCRIPTION", length = 45)
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "group",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+
+    )
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
