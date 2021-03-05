0078bcf2414ff2c1969961a86314d80668a7cdfc
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-03-05 20:33:15 +0100

    Test checking relations User - Cart created

--- src/test/java/com/kodilla/ecommercee/domain/UserTest.java
+++ src/test/java/com/kodilla/ecommercee/domain/UserTest.java
@@ -7,6 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 @SpringBootTest
@@ -35,5 +38,33 @@ public class UserTest {
 
         assertNotEquals(0, id);
 
+        userDao.deleteById(id);
+    }
+
+    @Test
+    public void testUserAndCartRelations() {
+
+        List<Cart> carts = new ArrayList<>();
+        Cart cart = new Cart();
+        carts.add(cart);
+        User user = User.builder()
+                .userName("John")
+                .password("John123")
+                .email("john.doe@test.com")
+                .build();
+        cart.setUser(user);
+        user.setCarts(carts);
+
+        userDao.save(user);
+        cartDao.save(cart);
+
+        int userId = user.getUserId();
+        int cartUserId = cart.getUser().getUserId();
+        int cartId = cart.getCartId();
+
+        assertEquals(cartUserId, userId);
+
+        cartDao.deleteById(cartId);
+        userDao.deleteById(userId);
     }
 }
