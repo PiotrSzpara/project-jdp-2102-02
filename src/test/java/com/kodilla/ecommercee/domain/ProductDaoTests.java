ed97fe6f0c971da3eff03bc5945796dc1f8fd812
Rafal Krismann <rkrismann33@gmail.com>
2021-03-06 01:54:31 +0000

    JDP210202-17

--- src/test/java/com/kodilla/ecommercee/domain/ProductDaoTests.java
+++ src/test/java/com/kodilla/ecommercee/domain/ProductDaoTests.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 
 import java.util.LinkedList;
@@ -19,6 +20,7 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class ProductDaoTests {
 
     @Autowired
