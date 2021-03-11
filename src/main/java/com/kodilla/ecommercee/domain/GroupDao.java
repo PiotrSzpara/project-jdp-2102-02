241782c99f93b8a5fc2e49ca07065d106dd161cf
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-03-11 23:30:55 +0100

    GroupDbService & GroupDao methods added

--- src/main/java/com/kodilla/ecommercee/domain/GroupDao.java
+++ src/main/java/com/kodilla/ecommercee/domain/GroupDao.java
@@ -1,12 +1,24 @@
 package com.kodilla.ecommercee.domain;
 
-import com.kodilla.ecommercee.domain.Group;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Transactional
 @Repository
 public interface GroupDao extends CrudRepository<Group, Integer> {
 
+    @Override
+    List<Group> findAll();
+
+    Optional<Group> findById(int id);
+
+    @Override
+    Group save(Group group);
+
+
+    void deleteById(int id);
 }
--- src/main/java/com/kodilla/ecommercee/service/GroupDbService.java
+++ src/main/java/com/kodilla/ecommercee/service/GroupDbService.java
@@ -1,2 +1,34 @@
-package com.kodilla.ecommercee.service;public class GroupDbService {
+package com.kodilla.ecommercee.service;
+
+import com.kodilla.ecommercee.domain.Group;
+import com.kodilla.ecommercee.domain.GroupDao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class GroupDbService {
+
+    @Autowired
+    private GroupDao groupDao;
+
+    public List<Group> getAllGroups() {
+        return groupDao.findAll();
+    }
+
+    public Optional<Group> getGroup(final int id) {
+        return groupDao.findById(id);
+    }
+
+    public Group saveGroup(final Group group) {
+        return groupDao.save(group);
+    }
+
+    public void deleteGroup(final int id) {
+        groupDao.deleteById(id);
+    }
 }
