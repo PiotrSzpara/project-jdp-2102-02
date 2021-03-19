871ab270b98c06fe7225735ec29f04f3192050da
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-03-19 16:49:45 +0100

    mapToUser and mapToUserDto amended to reflect constructors and mapToUserDtoList added

--- src/main/java/com/kodilla/ecommercee/mapper/UserMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/UserMapper.java
@@ -3,12 +3,16 @@ package com.kodilla.ecommercee.mapper;
 import com.kodilla.ecommercee.domain.*;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 public class UserMapper {
 
     public User mapToUser(final UserDto userDto) {
         return new User(
+                userDto.getUserId(),
                 userDto.getUserName(),
                 userDto.getEmail()
         );
@@ -16,8 +20,15 @@ public class UserMapper {
 
     public UserDto mapToUserDto(final User user) {
         return new UserDto (
+                user.getUserId(),
                 user.getUserName(),
                 user.getEmail()
         );
+    }
+
+    public List<UserDto> mapToUserDtoList(final List<User> users) {
+        return users.stream()
+                .map(this::mapToUserDto)
+                .collect(Collectors.toList());
     }
 }
